c12930ab61b54d2f0df03477fa472a887979920e
nighthowl <nighthowl0325@gmail.com>
2019-05-01 09:27:43 -0400

    Make video fullscreen Attempt 2

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 let capture;
 
 function setup() {
-    createCanvas(1280, 720);
+    createCanvas(1920, 1080);
     capture = createCapture(VIDEO);
     capture.size(1280, 720);
     capture.hide();
